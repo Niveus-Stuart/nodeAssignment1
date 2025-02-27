0dc0503bd5c5f1d780e7e368d1fb94bad318f229
Stuart Olivera <stuart.olivera@niveussolutions.com>
2025-02-27 10:28:31 +0530

    commit to create a sample file

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ const message = () => {
 message()
   .then((message) => console.log(message))
   .catch((err) => console.log(err));
+
+const fs = require("fs").promises;
+
+const createAFile = async () => {
+  try {
+    await fs.writeFile(
+      "sampleFile.txt",
+      "This is a sample file with some random message."
+    );
+    console.log("file created successfully");
+  } catch (error) {
+    console.log("something went wrong ", error);
+  }
+};
+
+createAFile();
